056cc1e65b8d1fdc221e23b7572b80f77628df31
AliTaha00 <ataha2021@fau.edu>
2024-12-25 04:33:29 -0500

    First Push

---
+++ PadelApp/PadelApp/PadelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadelAppApp.swift
+//  PadelApp
+//
+//  Created by Ali Taha on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct PadelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
